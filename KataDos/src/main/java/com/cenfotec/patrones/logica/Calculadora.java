5da41c96a81d3559dd70f0d8b8576764200aac84
SilverKnightAnnt <35677091+SilverKnightAnnt@users.noreply.github.com>
2018-02-12 19:24:31 -0600

    Solucionado el Test número 2
    En el caso anterior siempre retornaba 0, por lo que si le mandábamos un 2 fallaba, pero ahora se le añadió un poco de lógica

--- KataDos/src/main/java/com/cenfotec/patrones/logica/Calculadora.java
+++ KataDos/src/main/java/com/cenfotec/patrones/logica/Calculadora.java
@@ -4,6 +4,26 @@ public class Calculadora {
 
 	public int generate(int number) {
 		
-		return 0;
+		int factor = 2;
+		
+		int factorLimite = (int)Math.sqrt(number);
+		
+		while (factor <= factorLimite)
+		{			
+			while (number % factor == 0 )
+			{				
+				number = number/factor;
+			}		
+			
+			if (factor==2)
+				factor++;
+			else
+				factor+=2;
+		}	
+		if (number == 0) {
+			factor = 0;
+		}
+		
+		return factor;
 	}
 }
