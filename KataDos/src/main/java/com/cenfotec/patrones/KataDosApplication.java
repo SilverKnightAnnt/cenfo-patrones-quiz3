51fff09fbed6b179e118f706f0d89e5797b7f55a
SilverKnightAnnt <35677091+SilverKnightAnnt@users.noreply.github.com>
2018-02-12 19:30:38 -0600

    Añadido UI
    Se añadió un poco de lógica en la UI para que el resultado se imprima en la pantalla

--- KataDos/src/main/java/com/cenfotec/patrones/KataDosApplication.java
+++ KataDos/src/main/java/com/cenfotec/patrones/KataDosApplication.java
@@ -3,10 +3,16 @@ package com.cenfotec.patrones;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.cenfotec.patrones.logica.Calculadora;
+
 @SpringBootApplication
 public class KataDosApplication {
 
+	static Calculadora calc = new Calculadora();
 	public static void main(String[] args) {
-		SpringApplication.run(KataDosApplication.class, args);
+		
+		 int factor = calc.generate(2);
+		   System.out.print(2+"=");
+		   System.out.print(factor);
 	}
 }
