ef21e14d05cc480af9f7382fdd2d769dc5b5707e
SilverKnightAnnt <35677091+SilverKnightAnnt@users.noreply.github.com>
2018-02-12 20:10:53 -0600

    Actualización del Test #3
    Como se está manejando un LinkedList<Integer> se debería actualizar los @Test para que puedan funcionar con este método

--- KataDos/src/main/java/com/cenfotec/patrones/KataDosApplication.java
+++ KataDos/src/main/java/com/cenfotec/patrones/KataDosApplication.java
@@ -1,5 +1,7 @@
 package com.cenfotec.patrones;
 
+import java.util.LinkedList;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,8 +13,15 @@ public class KataDosApplication {
 	static Calculadora calc = new Calculadora();
 	public static void main(String[] args) {
 		
-		 int factor = calc.generate(4);
+		LinkedList<Integer> factores = calc.generate(4);
 		   System.out.print(4+"=");
-		   System.out.print(factor);
+		   for (Integer factor: factores)
+		   {
+			   System.out.print(factor);
+			   
+			   if (factor != factores.getLast())
+				   System.out.print("*");
+		   }
+		   System.out.println();
 	}
 }
--- KataDos/src/test/java/com/cenfotec/patrones/logica/CalculadoraTest.java
+++ KataDos/src/test/java/com/cenfotec/patrones/logica/CalculadoraTest.java
@@ -2,25 +2,34 @@ package com.cenfotec.patrones.logica;
 
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+
 import org.junit.Test;
 
 public class CalculadoraTest {
 
 	@Test
 	public void pruebaIntVacio() {
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		list.add(0);
 		Calculadora calc = new Calculadora();
-		assertEquals(0, calc.generate(0));
+		assertEquals(list, calc.generate(0));
 	}
 	
 	@Test 
 	public void pruebaInt() {
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		list.add(2);
 		Calculadora calc = new Calculadora();
-		assertEquals(2, calc.generate(2));
+		assertEquals(list, calc.generate(2));
 	}
 	
 	@Test 
 	public void pruebaIntVariosFactoresPrimos() {
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		list.add(2);
+		list.add(2);		
 		Calculadora calc = new Calculadora();
-		assertEquals(2+"*"+2, calc.generate(4));
+		assertEquals(list, calc.generate(4));
 	}
 }
