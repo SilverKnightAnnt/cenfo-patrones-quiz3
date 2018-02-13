494dd0cd7d95d04a138488a8c0b7b3cfeceef295
SilverKnightAnnt <35677091+SilverKnightAnnt@users.noreply.github.com>
2018-02-12 20:42:41 -0600

    Actualización de la UI
    Ahora el usuario puede ingresar cualquier número que el desee, para poder obtener los factores primos.

--- KataDos/src/main/java/com/cenfotec/patrones/KataDosApplication.java
+++ KataDos/src/main/java/com/cenfotec/patrones/KataDosApplication.java
@@ -1,5 +1,6 @@
 package com.cenfotec.patrones;
 
+import java.io.*;
 import java.util.LinkedList;
 
 import org.springframework.boot.SpringApplication;
@@ -10,18 +11,40 @@ import com.cenfotec.patrones.logica.Calculadora;
 @SpringBootApplication
 public class KataDosApplication {
 
-	static Calculadora calc = new Calculadora();
+	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	static PrintStream out = new PrintStream(System.out);
+	static Calculadora calc = new Calculadora();	
+	
 	public static void main(String[] args) {
+			pedirValor();
+	}
+	
+	static void pedirValor() {
 		
-		LinkedList<Integer> factores = calc.generate(4);
-		   System.out.print(4+"=");
+		int number = 0;
+		out.println("Ingrese el valor para el cual desea determinar los factores primos: \n");
+		try {
+			number = Integer.parseInt(in.readLine());
+			imprimirFactoresPrimos(number);
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {			
+			e.printStackTrace();
+		}
+		
+	}
+	
+	static void imprimirFactoresPrimos(int number) {
+		
+		LinkedList<Integer> factores = calc.generate(number);
+		   out.print("\nResultado de "+ number + " = ");
 		   for (Integer factor: factores)
 		   {
-			   System.out.print(factor);
+			   out.print(factor);
 			   
 			   if (factor != factores.getLast())
-				   System.out.print("*");
+				   out.print("*");
 		   }
-		   System.out.println();
+		   out.println();
 	}
 }
